133d1628091c5cb111c749864d3831b04deb84d1
adam malantonio <malantoa@lafayette.edu>
2019-08-20 16:30:13 -0400

    use appropriate row variable + date name for newspaper resovler

--- lib/spot_migration/resolvers/file_system.rb
+++ lib/spot_migration/resolvers/file_system.rb
@@ -16,7 +16,7 @@ module SpotMigration
 
     class Newspaper < FileSystem
       def call(_id, csv_row)
-        date = Array(row['dc:date']).first
+        date = Array(csv_row['date_issued']).first
         parsed_date = DateTime.parse(date).strftime('%Y_%m_%d')
         File.join(asset_base, "lafayette_newspaper_#{parsed_date}_OBJ.pdf")
       end
